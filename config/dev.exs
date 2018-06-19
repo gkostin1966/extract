be41995509fa80f03e65377226b5ca9dec793342
Greg Kostin <gkostin@umich.edu>
2018-06-19 09:44:14 -0400

    config postgres

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :extract, Extract.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "extract",
+  password: "extract",
   database: "extract_dev",
   hostname: "localhost",
   pool_size: 10
